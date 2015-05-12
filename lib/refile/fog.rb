4aead72d7b657f4c1c70f484d6e78293d674fbe0
Jonas Nicklas <jonas.nicklas@gmail.com>
2015-05-12 17:57:11 +0200

    Update confirmation error

--- lib/refile/fog.rb
+++ lib/refile/fog.rb
@@ -58,7 +58,7 @@ module Refile
       end
 
       def clear!(confirm = nil)
-        raise ArgumentError, "are you sure? this will remove all files in the backend, call as `clear!(:confirm)` if you're sure you want to do this" unless confirm == :confirm
+        raise Refile::Confirm unless confirm == :confirm
         @directory.files.select { |f| f.key.start_with?(@prefix.to_s) }.each(&:destroy)
       end
 
